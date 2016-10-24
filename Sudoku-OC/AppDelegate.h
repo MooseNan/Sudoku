fe08979c894ce814aeb37df02a0ef8f6855e70f9
Nancy <nan.li@coupang.com>
2016-10-24 17:20:42 +0800

    Initial Commit

---
+++ Sudoku-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sudoku-OC
+//
+//  Created by Li Nan on 16/10/24.
+//  Copyright © 2016年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sudoku-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sudoku-OC
+//
+//  Created by Li Nan on 16/10/24.
+//  Copyright © 2016年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
